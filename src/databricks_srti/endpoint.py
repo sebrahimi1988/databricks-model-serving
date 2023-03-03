add121816b8e1921226d77ff3b27c2325f9a10b3
Rafael Pierre <rafael.pierre@databricks.com>
2023-03-03 10:44:43 +0100

    docstring

--- src/databricks_srti/endpoint.py
+++ src/databricks_srti/endpoint.py
@@ -18,7 +18,6 @@ class EndpointApiClient:
             token: Access Token for interacting with your Databricks Workspace.
         """
         self.base_url = base_url
-        self.endpoints_base_path = "api/2.0/serving-endpoints/{}"
         self.token = token
 
     def create_inference_endpoint(
@@ -31,45 +30,105 @@ class EndpointApiClient:
             endpoint_name: Serving endpoint name.
             served_models: List of model names that will be deployed.
         """
-        self.endpoints_path = self.endpoints_base_path.format(endpoint_name)
+
         data = {"name": endpoint_name, "config": {"served_models": served_models}}
         return self._post("api/2.0/serving-endpoints", data)
 
-    def get_inference_endpoint(self, endpoint_name):
+    def get_inference_endpoint(self, endpoint_name: str) -> Dict:
+        """
+            Gets info on the inference endpoint.
+
+            endpoint_name: Serving endpoint name.
+        """
+
         return self._get(f"api/2.0/serving-endpoints/{endpoint_name}")
 
-    def list_inference_endpoints(self):
+    def list_inference_endpoints(self) -> Dict:
+        """Lists all running inference endpoints."""
+
         return self._get("api/2.0/serving-endpoints")
 
-    def update_served_models(self, endpoint_name, served_models, traffic_config=None):
+    def update_served_models(
+        self,
+        endpoint_name: str,
+        served_models: List[str],
+        traffic_config: Dict = None
+    ):
+        """
+            Updates served models with the specified traffic_config.
+            
+            endpoint_name: Serving endpoint name.
+            served_models: List of served models.
+            traffic_config: New traffic split configuration.
+        """
+
         if traffic_config is None:
             data = data = {"served_models": served_models}
         else:
             data = {"served_models": served_models, "traffic_config": traffic_config}
-        return self._put(f"{self.endpoints_base_path}/config", data)
+        return self._put(f"api/2.0/serving-endpoints/{endpoint_name}/config", data)
 
-    def delete_inference_endpoint(self, endpoint_name):
-        return self._delete(self.endpoints_base_path)
+    def delete_inference_endpoint(self, endpoint_name: str) -> Dict:
+        """
+            Deletes an inference endpoint.
+            
+            endpoint_name: Serving endpoint name.
+        """
 
-    # Making predictions
+        return self._delete(f"api/2.0/serving-endpoints/{endpoint_name}")
 
-    def query_inference_endpoint(self, endpoint_name, data):
+    def query_inference_endpoint(self, endpoint_name: str, data: Dict) -> Dict:
+        """
+            Makes HTTP requests to an inference endpoint.
+            
+            endpoint_name: Serving endpoint name.
+            data: Payload containing the data expected by the model.
+        """
+
         return self._post(f"realtime-inference/{endpoint_name}/invocations", data)
 
     # Debugging
 
-    def get_served_model_build_logs(self, endpoint_name, served_model_name):
+    def get_served_model_build_logs(
+        self,
+        endpoint_name: str,
+        served_model_name: str
+    ) -> Dict:
+        """
+            Gets the build logs for the specified endpoint/model.
+            
+            endpoint_name: Serving endpoint name.
+            served_model_name: Served model name.
+        """
+
         return self._get(
-            f"{self.endpoints_base_path}/served-models/{served_model_name}/build-logs"
+            f"api/2.0/serving-endpoints/{endpoint_name}/served-models/{served_model_name}/build-logs"
         )
 
-    def get_served_model_server_logs(self, endpoint_name, served_model_name):
+    def get_served_model_server_logs(
+        self,
+        endpoint_name: str,
+        served_model_name: str
+    ) -> Dict:
+        """
+            Gets the server logs for the specified endpoint/model.
+            
+            endpoint_name: Serving endpoint name.
+            served_model_name: Served model name.
+        """
+
         return self._get(
-            f"{self.endpoints_base_path}/served-models/{served_model_name}/logs"
+            f"api/2.0/serving-endpoints/{endpoint_name}/served-models/{served_model_name}/logs"
         )
 
-    def get_inference_endpoint_events(self, endpoint_name):
-        return self._get(f"{self.endpoints_base_path}/events")
+    def get_inference_endpoint_events(self, endpoint_name: str) -> Dict:
+        """
+            Gets the build endpoint events for the specified endpoint.
+            
+            endpoint_name: Serving endpoint name.
+        """
+
+        return self._get(f"api/2.0/serving-endpoints/{endpoint_name}/events")
 
     def _get(self, uri) -> Dict:
         url = f"{self.base_url}/{uri}"
